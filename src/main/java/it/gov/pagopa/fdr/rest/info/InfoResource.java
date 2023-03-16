2beb49f169b15eba0766b983062afdd2422eac77
Marco Meretta <marco.meretta@gmail.com>
2023-03-16 18:56:14 +0100

    ff

--- src/main/java/it/gov/pagopa/fdr/rest/info/InfoResource.java
+++ src/main/java/it/gov/pagopa/fdr/rest/info/InfoResource.java
@@ -1,39 +1,32 @@
 package it.gov.pagopa.fdr.rest.info;
 
 import it.gov.pagopa.fdr.rest.info.response.Info;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import org.jboss.logging.MDC;
 
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-
 @Path("/info")
 public class InfoResource {
 
-    @Inject
-    Logger log;
+  @Inject Logger log;
 
-    @ConfigProperty(name = "quarkus.application.name", defaultValue = "FDR")
-    String name;
+  @ConfigProperty(name = "quarkus.application.name", defaultValue = "FDR")
+  String name;
 
-    @ConfigProperty(name = "quarkus.application.version", defaultValue = "0.0.0")
-    String version;
+  @ConfigProperty(name = "quarkus.application.version", defaultValue = "0.0.0")
+  String version;
 
-    @ConfigProperty(name = "app.environment", defaultValue = "dev")
-    String environment;
+  @ConfigProperty(name = "app.environment", defaultValue = "dev")
+  String environment;
 
-    @GET
-    public Info hello() {
-        MDC.put("terra","prova");
-        log.info("info");
+  @GET
+  public Info hello() {
+    MDC.put("terra", "prova");
+    log.info("info");
 
-        return Info.builder()
-                .name(name)
-                .version(version)
-                .environment(environment)
-                .build();
-    }
+    return Info.builder().name(name).version(version).environment(environment).build();
+  }
 }
-
