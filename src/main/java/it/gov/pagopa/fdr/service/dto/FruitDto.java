19894b96f0ee6f1259f95dcc898e2c612ffd9726
Marco Meretta <marco.meretta@gmail.com>
2023-03-20 18:50:16 +0100

    google format

--- src/main/java/it/gov/pagopa/fdr/service/dto/FruitDto.java
+++ src/main/java/it/gov/pagopa/fdr/service/dto/FruitDto.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class FruitDto {
 
-    private String name;
-    private String description;
+  private String name;
+  private String description;
 }
