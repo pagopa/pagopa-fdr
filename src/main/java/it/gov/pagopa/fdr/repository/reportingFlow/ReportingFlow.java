4a4fea3ba0eddeacc04c011753937a2c3cd3700f
Marco Meretta <marco.meretta@gmail.com>
2023-04-22 21:32:22 +0200

    wip upload

--- src/main/java/it/gov/pagopa/fdr/repository/reportingFlow/ReportingFlow.java
+++ src/main/java/it/gov/pagopa/fdr/repository/reportingFlow/ReportingFlow.java
@@ -1,28 +1,6 @@
 package it.gov.pagopa.fdr.repository.reportingFlow;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
-import java.time.Instant;
-import java.util.List;
 
 @MongoEntity(collection = "reporting_flow")
-public class ReportingFlow extends AbstractReportingFlow {
-
-  public Long revision;
-  public Instant created;
-  public Instant updated;
-
-  public String reportingFlow;
-  public Instant dateReportingFlow;
-
-  public Sender sender;
-  public Receiver receiver;
-
-  public String regulation;
-  public Instant dateRegulation;
-
-  public String bicCodePouringBank;
-
-  public List<Pagamento> payments;
-
-  public ReportingFlowStatusEnum status;
-}
+public class ReportingFlow extends AbstractReportingFlow {}
