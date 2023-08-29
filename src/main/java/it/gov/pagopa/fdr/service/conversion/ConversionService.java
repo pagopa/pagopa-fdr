7aef7aa7e1fdd70d76164982366af639e571a677
marcomeretta <121177306+marcomeretta@users.noreply.github.com>
2023-08-29 14:03:10 +0200

    feat: set b64 to message queue (#31)
    * test enc
    
    * test enc
    
    * test enc

--- src/main/java/it/gov/pagopa/fdr/service/conversion/ConversionService.java
+++ src/main/java/it/gov/pagopa/fdr/service/conversion/ConversionService.java
@@ -1,11 +1,15 @@
 package it.gov.pagopa.fdr.service.conversion;
 
+import com.azure.core.util.BinaryData;
 import com.azure.storage.queue.QueueClient;
 import com.azure.storage.queue.QueueClientBuilder;
+import com.azure.storage.queue.QueueMessageEncoding;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.fdr.service.conversion.message.FdrMessage;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import lombok.SneakyThrows;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
@@ -28,7 +32,11 @@ public class ConversionService {
   @SneakyThrows
   public void init() {
     QueueClient queueClient =
-        new QueueClientBuilder().connectionString(connectStr).queueName(queueName).buildClient();
+        new QueueClientBuilder()
+            .connectionString(connectStr)
+            .queueName(queueName)
+            .messageEncoding(QueueMessageEncoding.BASE64)
+            .buildClient();
     queueClient.createIfNotExists();
 
     log.infof("Queue conversion init. Queue name [%s]", queueName);
@@ -45,7 +53,10 @@ public class ConversionService {
       log.infof(
           "Send message. Queue name [%s], pspId [%s], fdr [%s]",
           queueName, fdrMessage.getPspId(), fdrMessage.getFdr());
-      this.queue.sendMessage(objectMapper.writeValueAsString(fdrMessage));
+      String rawString = objectMapper.writeValueAsString(fdrMessage);
+      String b64String =
+          Base64.getEncoder().encodeToString(rawString.getBytes(StandardCharsets.UTF_8));
+      this.queue.sendMessage(BinaryData.fromString(b64String));
     }
   }
 }
