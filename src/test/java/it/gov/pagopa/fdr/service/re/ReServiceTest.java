0760e3ea08ff86de8013c9bcd677260eef76776a
grassog <giovanni.grasso@tasgroup.eu>
2023-09-11 18:25:23 +0200

    Added more test

--- src/test/java/it/gov/pagopa/fdr/service/re/ReServiceTest.java
+++ src/test/java/it/gov/pagopa/fdr/service/re/ReServiceTest.java
@@ -11,77 +11,82 @@ import io.quarkiverse.mockserver.test.MockServerTestResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
+import it.gov.pagopa.fdr.exception.AppErrorCodeMessageEnum;
+import it.gov.pagopa.fdr.exception.AppException;
 import it.gov.pagopa.fdr.service.re.model.*;
 import it.gov.pagopa.fdr.test.util.AzuriteResource;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 
 import java.lang.reflect.Field;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.bson.assertions.Assertions.fail;
 
 @QuarkusTest
 @QuarkusTestResource(MockServerTestResource.class)
 @QuarkusTestResource(AzuriteResource.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ReServiceTest {
-
-    @Inject
-    org.jboss.logging.Logger log;
-
     @Inject
     ObjectMapper objectMapper;
-
     @InjectMock
-    ReService mock;
-
+    ReService reServiceMock;
     @ConfigProperty(name = "queue.conversion.connect-str")
     String connString;
-
     @ConfigProperty(name = "%dev.blob.re.containername")
     static String blobName;
-    BlobContainerClient blobContainerClientMock;
+    BlobContainerClient blobContainerClient;
     static BlobServiceClient blobServiceClient;
     static EventHubProducerClient producerMock;
+    static ReInterface reInterface;
+    Field field1;
     Field field2;
-    @BeforeEach
+    Field field3;
+    Field field4;
+    Field logField;
+    Field objectMapperField;
+    @BeforeAll
     void init() throws NoSuchFieldException, IllegalAccessException {
-
-        Field field1 = ReService.class.getDeclaredField("producer");
+        field1 = ReService.class.getDeclaredField("producer");
         field2 = ReService.class.getDeclaredField("blobContainerClient");
-        Field field3 = ReService.class.getDeclaredField("blobContainerName");
-        Field field4 = ReService.class.getDeclaredField("eHubName");
-
+        field3 = ReService.class.getDeclaredField("blobContainerName");
+        field4 = ReService.class.getDeclaredField("eHubName");
         blobServiceClient = new BlobServiceClientBuilder().connectionString(connString).buildClient();
-        blobContainerClientMock = blobServiceClient.createBlobContainerIfNotExists(blobName);
+        blobContainerClient = blobServiceClient.createBlobContainerIfNotExists(blobName);
         producerMock = Mockito.mock(EventHubProducerClient.class);
 
         field1.setAccessible(true);
         field2.setAccessible(true);
 
-        field1.set(mock, producerMock);
-        field2.set(mock, blobContainerClientMock);
-        field3.set(mock, "blobcontainerre");
-        field4.set(mock, "eventHub");
+        field2.set(reServiceMock, blobContainerClient);
+        field3.set(reServiceMock, "blobcontainerre");
+        field4.set(reServiceMock, "eventHub");
 
-        Field logField = ReService.class.getDeclaredField("log");
-        Field objectMapperField = ReService.class.getDeclaredField("objectMapper");
-        logField.set(mock, Logger.getLogger(ReService.class));
-        objectMapperField.set(mock, objectMapper);
+        logField = ReService.class.getDeclaredField("log");
+        objectMapperField = ReService.class.getDeclaredField("objectMapper");
+        logField.set(reServiceMock, Logger.getLogger(ReService.class));
+        objectMapperField.set(reServiceMock, objectMapper);
 
-        Mockito.doNothing().when(mock).init();
-        Mockito.doNothing().when(mock).publishEvents(Mockito.any());
-        Mockito.doCallRealMethod().when(mock).sendEvent(Mockito.any(ReInterface.class));
-        Mockito.doCallRealMethod().when(mock).writeBlobIfExist(Mockito.any());
+        Mockito.doNothing().when(reServiceMock).init();
+        Mockito.doNothing().when(reServiceMock).publishEvents(Mockito.any());
+        Mockito.doCallRealMethod().when(reServiceMock).sendEvent(Mockito.any(ReInterface.class));
+        Mockito.doCallRealMethod().when(reServiceMock).writeBlobIfExist(Mockito.any());
     }
-
-    @Test
-    public void testSend() {
-        ReInterface reInterface =
+    @BeforeEach
+    public void setReInterface() throws IllegalAccessException {
+        field1.set(reServiceMock, producerMock);
+        reInterface =
                 ReInterface.builder()
                         .uniqueId("123")
                         .appVersion(AppVersionEnum.FDR003)
@@ -96,67 +101,107 @@ public class ReServiceTest {
                         .fdr("1")
                         .organizationId("1")
                         .fdrAction(FdrActionEnum.GET_FDR)
-                        .build()
-        ;
+                        .build();
+        Mockito.clearInvocations(reServiceMock);
+        Mockito.clearInvocations(producerMock);
+    }
 
-        mock.sendEvent(reInterface);
-        Mockito.verify(mock, Mockito.times(1)).sendEvent(reInterface);
-        Mockito.verify(mock, Mockito.times(1)).writeBlobIfExist(Mockito.any());
-        Mockito.verify(mock, Mockito.times(1)).publishEvents(Mockito.any());
+    @Test
+    public void testSend() {
+        reServiceMock.sendEvent(reInterface);
+        Mockito.verify(reServiceMock, Mockito.times(1)).writeBlobIfExist(Mockito.any());
+        Mockito.verify(reServiceMock, Mockito.times(1)).publishEvents(Mockito.any());
     }
     @Test
-    public void testSendActionInfo() {
-        ReInterface reInterface =
-            ReInterface.builder()
-                .uniqueId("123")
-                .appVersion(AppVersionEnum.FDR003)
-                .created(Instant.now())
-                .sessionId("sessionId")
-                .eventType(EventTypeEnum.INTERFACE)
-                .httpType(HttpTypeEnum.RES)
-                .httpMethod("GET")
-                .httpUrl("requestPath")
-                .payload("responsePayload")
-                .pspId("1")
-                .fdr("1")
-                .organizationId("1")
-                .fdrAction(FdrActionEnum.INFO)
-                .build();
+    public void testSend_ActionInfo() {
+        reInterface.setFdrAction(FdrActionEnum.INFO);
+        reServiceMock.sendEvent(reInterface);
+        Mockito.verify(reServiceMock, Mockito.times(0)).writeBlobIfExist(Mockito.any());
+        Mockito.verify(reServiceMock, Mockito.times(0)).publishEvents(Mockito.any());
+    }
 
-        mock.sendEvent(reInterface);
-        Mockito.verify(mock, Mockito.times(1)).sendEvent(reInterface);
-        Mockito.verify(mock, Mockito.times(0)).writeBlobIfExist(Mockito.any());
-        Mockito.verify(mock, Mockito.times(0)).publishEvents(Mockito.any());
+//    @Test
+//    public void testSend_Producer_BlobContainerClient() throws IllegalAccessException {
+//        field1.set(reServiceMock,null);
+//        reServiceMock.sendEvent(reInterface);
+//        Mockito.verify(reServiceMock, Mockito.times(0)).writeBlobIfExist(Mockito.any());
+//        Mockito.verify(reServiceMock, Mockito.times(0)).publishEvents(Mockito.any());
+//    }
+
+    @Test
+    public void testJsonProcessingException() {
+        reServiceMock.sendEvent(null);
+        Mockito.verify(reServiceMock, Mockito.times(0)).writeBlobIfExist(Mockito.any());
+        Mockito.verify(reServiceMock, Mockito.times(0)).publishEvents(Mockito.any());
+    }
+    @Test
+    public void testWriteBlobIfExist_NoReInstance() {
+        reServiceMock.writeBlobIfExist(ReInternal.builder().build());
+        Mockito.verify(Mockito.spy(blobContainerClient), Mockito.times(0)).getBlobClient(Mockito.any());
+    }
+
+    @Test
+    public void testWriteBlobIfExist_BodyStringBlank() {
+        reInterface.setPayload("");
+        reServiceMock.writeBlobIfExist(reInterface);
+        Mockito.verify(Mockito.spy(blobContainerClient), Mockito.times(0)).getBlobClient(Mockito.any());
     }
 
     @Test
     public void testPublishEvent() throws JsonProcessingException {
-        Mockito.doCallRealMethod().when(mock).publishEvents(Mockito.any());
+        Mockito.doCallRealMethod().when(reServiceMock).publishEvents(Mockito.any());
         EventDataBatch eventDataBatch = Mockito.mock(EventDataBatch.class);
         Mockito.when(producerMock.createBatch()).thenReturn(eventDataBatch);
         Mockito.when(eventDataBatch.tryAdd(Mockito.any())).thenReturn(true);
         Mockito.when(eventDataBatch.getCount()).thenReturn(1);
         List<EventData> eventDataList = new ArrayList<>();
         Mockito.doNothing().when(producerMock).send((EventDataBatch) Mockito.any());
-        ReInterface reInterface =
-                ReInterface.builder()
-                        .uniqueId("123")
-                        .appVersion(AppVersionEnum.FDR003)
-                        .created(Instant.now())
-                        .sessionId("sessionId")
-                        .eventType(EventTypeEnum.INTERFACE)
-                        .httpType(HttpTypeEnum.RES)
-                        .httpMethod("GET")
-                        .httpUrl("requestPath")
-                        .payload("responsePayload")
-                        .pspId("1")
-                        .fdr("1")
-                        .organizationId("1")
-                        .fdrAction(FdrActionEnum.INFO)
-                        .build();
         EventData eventData = new EventData(objectMapper.writeValueAsString(reInterface));
         eventDataList.add(eventData);
-        mock.publishEvents(eventDataList);
+        reServiceMock.publishEvents(eventDataList);
         Mockito.verify(producerMock,Mockito.times(1)).send((EventDataBatch) Mockito.any());
+    }
+
+    @Test
+    public void testPublishEvent_FullBatch() throws JsonProcessingException {
+        AtomicInteger counter = new AtomicInteger();
+        Mockito.doCallRealMethod().when(reServiceMock).publishEvents(Mockito.any());
+        EventDataBatch eventDataBatch = Mockito.mock(EventDataBatch.class);
+        Mockito.when(producerMock.createBatch()).thenReturn(eventDataBatch);
+        Mockito.when(eventDataBatch.tryAdd(Mockito.any())).thenAnswer(
+                invocation -> {
+                    if(counter.get() == 1){
+                        return true;
+                    }else{
+                        counter.set(1);
+                        return false;
+                    }
+                });
+        Mockito.when(eventDataBatch.getCount()).thenReturn(1);
+        List<EventData> eventDataList = new ArrayList<>();
+        Mockito.doNothing().when(producerMock).send((EventDataBatch) Mockito.any());
+        EventData eventData = new EventData(objectMapper.writeValueAsString(reInterface));
+        eventDataList.add(eventData);
+        reServiceMock.publishEvents(eventDataList);
+        Mockito.verify(producerMock,Mockito.times(2)).send((EventDataBatch) Mockito.any());
+    }
+
+    @Test
+    public void testPublishEvent_TooLarge() throws JsonProcessingException {
+        Mockito.doCallRealMethod().when(reServiceMock).publishEvents(Mockito.any());
+        EventDataBatch eventDataBatch = Mockito.mock(EventDataBatch.class);
+        Mockito.when(producerMock.createBatch()).thenReturn(eventDataBatch);
+        Mockito.when(eventDataBatch.tryAdd(Mockito.any())).thenReturn(false);
+        Mockito.when(eventDataBatch.getCount()).thenReturn(1);
+        List<EventData> eventDataList = new ArrayList<>();
+        Mockito.doNothing().when(producerMock).send((EventDataBatch) Mockito.any());
+        EventData eventData = new EventData(objectMapper.writeValueAsString(reInterface));
+        eventDataList.add(eventData);
+        try {
+            reServiceMock.publishEvents(eventDataList);
+            fail();
+        }catch(AppException e){
+            Assert.assertEquals(AppErrorCodeMessageEnum.EVENT_HUB_RE_TOO_LARGE, e.getCodeMessage());
+        }
     }
 }
