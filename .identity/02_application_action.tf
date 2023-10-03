85a622e4d88346cadf6375ac7f33733641494586
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-10-03 17:35:55 +0200

    wip

--- .identity/02_application_action.tf
+++ .identity/02_application_action.tf
@@ -1,5 +1,5 @@
 module "identity-ci" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//identity-github-runner?ref=gh-identity-module"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//identity_github_runner?ref=gh-identity-module"
 
   resource_group_name = local.container_app_environment.resource_group
   location            = "westeurope"
@@ -9,6 +9,8 @@ module "identity-ci" {
     repository = local.github.repository
     subject    = var.env
   }
+
+  tags = {}
 }
 
 #module "github_runner_app" {
@@ -28,7 +30,7 @@ module "identity-ci" {
 resource "null_resource" "github_runner_app_permissions_to_namespace" {
   triggers = {
     aks_id               = data.azurerm_kubernetes_cluster.aks.id
-    service_principal_id = module.identity-ci.client_id
+    service_principal_id = module.identity-ci.user_managed_identity.object_id
     namespace            = local.domain
     version              = "v2"
   }
@@ -57,38 +59,38 @@ resource "null_resource" "github_runner_app_permissions_to_namespace" {
 resource "azurerm_role_assignment" "environment_terraform_storage_account" {
   scope                = data.azurerm_storage_account.tf_storage_account.id
   role_definition_name = "Contributor"
-  principal_id         = module.identity-ci.object_id
+  principal_id         = module.identity-ci.user_managed_identity.object_id
 }
 
 resource "azurerm_role_assignment" "environment_terraform_resource_group_apim" {
   scope                = data.azurerm_resource_group.apim_resource_group.id
   role_definition_name = "Contributor"
-  principal_id         = module.identity-ci.object_id
+  principal_id         = module.identity-ci.user_managed_identity.object_id
 }
 
 resource "azurerm_role_assignment" "environment_terraform_resource_group_dashboards" {
   scope                = data.azurerm_resource_group.dashboards.id
   role_definition_name = "Contributor"
-  principal_id         = module.identity-ci.object_id
+  principal_id         = module.identity-ci.user_managed_identity.object_id
 }
 
 resource "azurerm_role_assignment" "environment_key_vault" {
   scope                = data.azurerm_key_vault.key_vault.id
   role_definition_name = "Reader"
-  principal_id         = module.identity-ci.object_id
+  principal_id         = module.identity-ci.user_managed_identity.object_id
 }
 
 resource "azurerm_role_assignment" "environment_key_vault_domain" {
   scope                = data.azurerm_key_vault.domain_key_vault.id
   role_definition_name = "Reader"
-  principal_id         = module.identity-ci.object_id
+  principal_id         = module.identity-ci.user_managed_identity.object_id
 }
 
 resource "azurerm_key_vault_access_policy" "ad_kv_group_policy" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = module.identity-ci.object_id
+  object_id = module.identity-ci.user_managed_identity.object_id
 
   key_permissions         = []
   secret_permissions      = ["Get", "List"]
@@ -100,7 +102,7 @@ resource "azurerm_key_vault_access_policy" "ad_domain_kv_group_policy" {
   key_vault_id = data.azurerm_key_vault.domain_key_vault.id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = module.identity-ci.object_id
+  object_id = module.identity-ci.user_managed_identity.object_id
 
   key_permissions         = []
   secret_permissions      = ["Get", "List"]
