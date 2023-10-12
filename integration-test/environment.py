d05a20b67b6f84b895d97bb13471f9afad62bf06
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-10-12 19:47:59 +0200

    happy case

---
+++ integration-test/environment.py
@@ -0,0 +1,51 @@
+import json
+from behave.model import Table
+import os
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def before_all(context):
+    # initialize precondition cache to avoid check systems up for each scenario
+    context.precondition_cache = set()
+
+    print('Global settings...')
+
+    more_userdata = json.load(open(os.path.join(context.config.base_dir + "/config/config.json")))
+    context.config.update_userdata(more_userdata)
+
+
+def before_feature(context, feature):
+    services = context.config.userdata.get("services")
+    # add heading
+    feature.background.steps[0].table = Table(headings=("name", "url", "healthcheck", "subscription_key"))
+    # add data in the tables
+    for system_name in services.keys():
+        row = (
+            system_name,
+            services.get(system_name).get("url", ""),
+            services.get(system_name).get("healthcheck", ""),
+            os.environ.get(services.get(system_name).get("subscription_key", ""))
+        )
+        feature.background.steps[0].table.add_row(row)
+
+
+def after_feature(context, feature):
+    global_configuration = context.config.userdata.get("global_configuration")
+
+
+def after_all(context):
+    print("After all disabled")
+    # db_selected = context.config.userdata.get("db_configuration").get('nodo_cfg')
+    # conn = db.getConnection(db_selected.get('host'), db_selected.get('database'), db_selected.get('user'), db_selected.get('password'),db_selected.get('port'))
+    #
+    # config_dict = getattr(context, 'configurations')
+    # for key, value in config_dict.items():
+    #     #print(key, value)
+    #     selected_query = utils.query_json(context, 'update_config', 'configurations').replace('value', value).replace('key', key)
+    #     db.executeQuery(conn, selected_query)
+    #
+    # db.closeConnection(conn)
+    # headers = {'Host': 'api.dev.platform.pagopa.it:443'}
+    # requests.get(utils.get_refresh_config_url(context), verify=False, headers=headers)
---
+++ integration-test/steps/steps.py
@@ -0,0 +1,86 @@
+from behave import *
+import logging
+import requests
+import datetime
+
+import utils as utils
+
+# Constants
+RESPONSE = "RES"
+REQUEST = "REQ"
+
+@given('systems up')
+def step_impl(context):
+    """
+        health check for defined systems
+    """
+    responses = True
+
+    if "systems up" not in context.precondition_cache:
+
+        for row in context.table:
+            logging.debug(f"calling: {row.get('name')} -> {row.get('url')}")
+
+            url = row.get("url") + row.get("healthcheck")
+            logging.debug(f"calling -> {url}")
+            # headers = {'Host': 'api.dev.platform.pagopa.it:443'}
+            resp = requests.get(url, verify=False)
+            logging.debug(f"response: {resp.status_code}")
+            # TODO remove comment
+            # responses &= (resp.status_code == 200)
+
+        if responses:
+            context.precondition_cache.add("systems up")
+
+    assert responses
+
+
+@given('an unique FdR {field_type} named {field_name}')
+def step_impl(context, field_type, field_name):
+    if field_type == 'name':
+        today = datetime.datetime.today()
+        fdr_name = today.strftime('%Y-%m-%d') + utils.get_global_conf(context, "psp") + "-" + today.strftime("%H%M%S%f")
+        setattr(context, field_name, fdr_name)
+    elif field_type == 'date':
+        today = datetime.datetime.today()
+        # TODO FIX: Bad request. Field [fdrDate] is [2023-10-12T19:42:33.768]. Expected ISO-8601 [2011-12-03T10:15:30Z] [2023-04-05T09:21:37.810000Z]"}]
+        setattr(context, field_name, today.isoformat(timespec='milliseconds'))
+
+
+@given('an FdR flow like {payload}')
+def step_impl(context, payload):
+    data = context.text or ""
+    data = utils.replace_local_variables(data, context)
+    data = utils.replace_global_variables(data, context)
+    setattr(context, payload, data)
+
+
+@when('PSP sends a {request_type} request to fdr-microservice with {payload}')
+def step_impl(context, request_type, payload):
+
+    subscription_key = utils.get_subscription_key(context, "fdr")
+    headers = {'Content-Type': 'application/json'}
+    if subscription_key is not None:
+        headers['Ocp-Apim-Subscription-Key'] = subscription_key
+
+    fdr_config = context.config.userdata.get("services").get("fdr")
+    endpoint_info = utils.get_fdr_url(request_type)
+    endpoint = utils.replace_local_variables(endpoint_info.get("endpoint"), context)
+    endpoint = utils.replace_global_variables(endpoint, context)
+    endpoint_info["endpoint"] = endpoint
+    url = fdr_config.get("url") + endpoint
+    response = utils.execute_request(url=url, method=endpoint_info.get("method"), headers=headers, payload=getattr(context, payload))
+    setattr(context, request_type + RESPONSE, response)
+
+
+@then('PSP receives the HTTP status code {http_status_code} to {request_type} request')
+def step_impl(context, http_status_code, request_type):
+    response = getattr(context, request_type + RESPONSE)
+    assert (response.status_code == int(http_status_code)), f"status_code {response.status_code}, expected {http_status_code}"
+
+@given('{test}')
+@when('{test}')
+@then('{test}')
+def step_impl(context):
+    print("TEST")
+    pass
