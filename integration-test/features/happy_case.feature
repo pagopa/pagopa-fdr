00b40f1597c4db75a33a24fa25e94acab8ff3786
Grasso Giovanni <giovanni.grasso@tasgroup.eu>
2023-10-16 17:07:45 +0200

    feat: added addPayment integration-test

--- integration-test/features/happy_case.feature
+++ integration-test/features/happy_case.feature
@@ -1,12 +1,10 @@
 Feature: Happy case
-#  Create a FdR
-#  Add 3 payments
-#  Publish FdR
 
   Background:
     Given systems up
 
 
+  @runnable
   Scenario: Create FdR
     Given an unique FdR name named flow_name
     And an unique FdR date named flow_date
@@ -32,25 +30,28 @@ Feature: Happy case
           "regulation": "SEPA - Bonifico xzy",
           "regulationDate": "$flow_date$",
           "bicCodePouringBank": "UNCRITMMXXX",
-          "totPayments": $number_of_payments$,
-          "sumPayments": $payments_amount$
+          "totPayments": 3,
+          "sumPayments": 0.03
         }
       """
-    When PSP sends create request to fdr-microservice with payload
+    When PSP sends a create request to fdr-microservice with payload
     Then PSP receives the HTTP status code 201 to create request
 
-
-  Scenario: Add payments
-    Given PSP should sends 3 payments to the FdR
-    And PSP should sends payments to the FdR whose sum is 300
-    And the Create FdR scenario executed successfully
-    When PSP adds 3 payments whose sum is 300 to the FdR named flow_name like payload
-    And PSP sends add_payments request to fdr-microservice with payload
-    Then PSP receives the HTTP status code 200 to add_payments request
-
-
   @runnable
-  Scenario: Publish FdR
-    Given the Add payments scenario executed successfully
-    When PSP sends publish request to fdr-microservice with None
-    Then PSP receives the HTTP status code 200 to publish request
+  Scenario Outline: Add Payment Fdr
+    Given The previously created flow flow_name
+    And a payment payload
+      """
+        {
+          "payments": [
+          ]
+        }
+      """
+    When PSP sends a addPayment request containing "<number>" payments
+    Then PSP receives the HTTP status code <http_status_code> to addPayment request
+  Examples:
+    | number | http_status_code |
+    | 1      | 200              |
+    | 10     | 200              |
+    | 1000   | 200              |
+    | 1001   | 400              |
--- integration-test/steps/steps.py
+++ integration-test/steps/steps.py
@@ -4,6 +4,9 @@ import requests
 import datetime
 
 import utils as utils
+import json
+
+shared_data = {"flow_name": None}
 
 # Constants
 RESPONSE = "RES"
@@ -27,7 +30,7 @@ def step_impl(context):
             headers = {'Content-Type': 'application/json'}
             if subscription_key is not None:
                 headers['Ocp-Apim-Subscription-Key'] = subscription_key
-#             headers = {'Host': 'api.dev.platform.pagopa.it:443'}
+# x            headers = {'Host': 'api.dev.platform.pagopa.it:443'}
             resp = requests.get(url, headers=headers, verify=False)
             logging.debug(f"response: {resp.status_code}")
             responses &= (resp.status_code == 200)
@@ -44,6 +47,7 @@ def step_impl(context, field_type, field_name):
         today = datetime.datetime.today()
         fdr_name = today.strftime('%Y-%m-%d') + utils.get_global_conf(context, "psp") + "-" + today.strftime("%H%M%S%f")
         setattr(context, field_name, fdr_name)
+        shared_data["flow_name"] = fdr_name
     elif field_type == 'date':
         today = datetime.datetime.today()
         setattr(context, field_name, today.strftime('%Y-%m-%dT%H:%M:%SZ'))
@@ -59,17 +63,16 @@ def step_impl(context, payload):
 
 @when('PSP sends a {request_type} request to fdr-microservice with {payload}')
 def step_impl(context, request_type, payload):
-
     subscription_key = utils.get_subscription_key(context, "fdr")
     headers = {'Content-Type': 'application/json'}
     if subscription_key is not None:
         headers['Ocp-Apim-Subscription-Key'] = subscription_key
-
     fdr_config = context.config.userdata.get("services").get("fdr")
     endpoint_info = utils.get_fdr_url(request_type)
     endpoint = utils.replace_local_variables(endpoint_info.get("endpoint"), context)
     endpoint = utils.replace_global_variables(endpoint, context)
     endpoint_info["endpoint"] = endpoint
+
     url = fdr_config.get("url") + endpoint
     response = utils.execute_request(url=url, method=endpoint_info.get("method"), headers=headers, payload=getattr(context, payload))
     setattr(context, request_type + RESPONSE, response)
@@ -79,6 +82,45 @@ def step_impl(context, request_type, payload):
 def step_impl(context, http_status_code, request_type):
     response = getattr(context, request_type + RESPONSE)
     assert (response.status_code == int(http_status_code)), f"status_code {response.status_code}, expected {http_status_code}"
+
+@given('The previously created flow {flow_name}')
+def step_impl(context,flow_name):
+    print("FlowName", shared_data.get("flow_name"))
+    setattr(context, flow_name, shared_data.get("flow_name"))
+
+
+@given('a payment {payload}')
+def step_impl(context, payload):
+    data = context.text or ""
+    data = utils.replace_local_variables(data, context)
+    data = utils.replace_global_variables(data, context)
+    setattr(context, payload, data)
+
+@when('PSP sends a addPayment request containing "{number}" payments')
+def step_impl(context, number):
+    payload=json.loads(context.payload)
+    for e in range(1,int(number)+1):
+        payment_obj = json.loads(utils.get_global_conf(context, "payment_payload"))
+        index=(shared_data.get("addPayment_lastIndex") or 0)+1
+        payment_obj["index"]= index
+        shared_data["addPayment_lastIndex"] = index
+        payload["payments"].append(payment_obj)
+    subscription_key = utils.get_subscription_key(context, "fdr")
+    headers = {'Content-Type': 'application/json'}
+    if subscription_key is not None:
+        headers['Ocp-Apim-Subscription-Key'] = subscription_key
+    fdr_config = context.config.userdata.get("services").get("fdr")
+    endpoint_info = utils.get_fdr_url("addPayment")
+    endpoint = utils.replace_local_variables(endpoint_info.get("endpoint"), context)
+    endpoint = utils.replace_global_variables(endpoint, context)
+    endpoint_info["endpoint"] = endpoint
+    print("Payload: ", payload)
+    url = fdr_config.get("url") + endpoint
+    response = utils.execute_request(url=url, method=endpoint_info.get("method"), headers=headers, payload=json.dumps(payload))
+    print("Response: ", response.text)
+    setattr(context, "addPayment" + RESPONSE, response)
+
+
 
 @given('{test}')
 @when('{test}')
