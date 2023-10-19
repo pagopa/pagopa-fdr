0c3a38d33d6bfe5a05f853bba266523d7cfa9537
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-10-19 10:48:48 +0200

    org happy case wip

--- integration-test/steps/utils.py
+++ integration-test/steps/utils.py
@@ -1,6 +1,35 @@
 import re
 import os
 import requests
+import logging
+import contextlib
+import string
+import random
+from http.client import HTTPConnection
+
+
+def debug_requests_on():
+    '''Switches on logging of the requests module.'''
+    HTTPConnection.debuglevel = 1
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
+def debug_requests_off():
+    '''Switches off logging of the requests module, might be some side-effects'''
+    HTTPConnection.debuglevel = 0
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.WARNING)
+    root_logger.handlers = []
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.WARNING)
+    requests_log.propagate = False
+
 
 def get_global_conf(context, field):
     return context.config.userdata.get("global_configuration").get(field)
@@ -32,17 +61,48 @@ def get_fdr_url(request_type=""):
             "endpoint": "/psps/#psp#/fdrs/$flow_name$",
             "method": "POST"
         },
-         "addPayment": {
+        "add_payments": {
             "endpoint": "/psps/#psp#/fdrs/$flow_name$/payments/add",
             "method": "PUT"
-         }
+        },
+        "del_payments": {
+            "endpoint": "/psps/#psp#/fdrs/$flow_name$/payments/del",
+            "method": "PUT"
+        },
+        "publish": {
+            "endpoint": "/psps/#psp#/fdrs/$flow_name$/publish",
+            "method": "POST"
+        },
+        "created_payments": {
+            "endpoint": "/psps/#psp#/fdrs/$flow_name$/payments",
+            "method": "GET"
+        },
+        "created_fdr": {
+            "endpoint": "/psps/#psp#/fdrs/$flow_name$",
+            "method": "GET"
+        },
+        "get_published": {
+            "endpoint": "/organizations/#organization#/fdrs/$flow_name$/revisions/$revision$/psps/#psp#",
+            "method": "GET"
+        },
+        "get_all_published": {
+            "endpoint": "/organizations/#organization#/fdrs",
+            "method": "GET"
+        },
+        "get_all_published_by_psp": {
+            "endpoint": "/organizations/#organization#/fdrs?pspId=#psp#",
+            "method": "GET"
+        },
+
     }
     return request_type_mapping.get(request_type)
 
 
 def execute_request(url, method, headers, payload=None):
-    return requests.request(method=method, url=url, headers=headers, data=payload, verify=False)
-
+    debug_requests_on()
+    req = requests.request(method=method, url=url, headers=headers, data=payload)
+    debug_requests_off()
+    return req
 
 
 def get_subscription_key(context, config):
@@ -51,3 +111,23 @@ def get_subscription_key(context, config):
         return os.getenv(data.get("subscription_key"))
 
     return None
+
+
+def generate_iuv():
+    return get_random_string(14)
+
+
+def generate_iur():
+    return get_random_string(10)
+
+
+def get_random_string(length):
+    return ''.join(random.choice(string.digits) for i in range(length))
+
+
+def append_to_query_params(context, query_param):
+    query_params = ""
+    if hasattr(context, "query_params"):
+        query_params = getattr(context, "query_params") + "&"
+    query_params += query_param
+    setattr(context, "query_params", query_params)
