fcbfa9a4c876cdc020b6d9facc9ca0fbe1c0b818
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-10-24 16:57:45 +0200

    wip

--- integration-test/run_test.sh
+++ integration-test/run_test.sh
@@ -1,9 +1,8 @@
 echo "Run test ..."
-#rm -rf results
+rm -rf results
 
 echo "Command to execute ..."
 echo "behave --format html -o reports/index.html --junit-directory=./reports --junit --tags=$TAGS --summary --show-timings -v"
-
 
 #behave --format html -o reports/index.html --junit-directory=./results --junit --tags=$TAGS --summary --show-timings -v
 behave --format allure_behave.formatter:AllureFormatter -o results --tags=$TAGS --summary --show-timings -v
