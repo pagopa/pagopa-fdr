a3c08aabaefb31079601e386dcb81582fb996822
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-10-26 17:01:20 +0200

    fix

--- integration-test/run_test.sh
+++ integration-test/run_test.sh
@@ -45,7 +45,7 @@ rm -rf results junit
 #echo "behave --format html -o reports/index.html $junit --tags=$TAGS --summary --show-timings -v"
 behave --format allure_behave.formatter:AllureFormatter -o results $junit --tags=$TAGS --summary --show-timings -v
 
-rm -rf results/history && cp -R reports/history results/history
+rm -rf results/history && cp -R reports/history results/history 2>/dev/null
 allure generate results -o reports --clean
 
 if ! [ -z $ENV ]
