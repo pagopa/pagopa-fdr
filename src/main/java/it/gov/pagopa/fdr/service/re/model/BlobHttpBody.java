b7fd669af4802120e825fdc9f1362d5f7fde5b2d
andrea-barchi <andrea.barchi@tasgroup.eu>
2024-01-23 14:42:15 +0100

    NOD-642 Fix test

--- src/main/java/it/gov/pagopa/fdr/service/re/model/BlobHttpBody.java
+++ src/main/java/it/gov/pagopa/fdr/service/re/model/BlobHttpBody.java
@@ -1,11 +1,15 @@
 package it.gov.pagopa.fdr.service.re.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BlobHttpBody {
 
   @BsonProperty("storage_account")
