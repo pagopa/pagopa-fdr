86f44b2a07ff31488078b902d814b864806874d1
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-01-17 17:24:55 +0100

    [PAGOPA-2528] feat: adding new entities that will replace old collections

---
+++ src/main/java/it/gov/pagopa/fdr/repository/entity/flow/FdrFlowEntity.java
@@ -0,0 +1,99 @@
+package it.gov.pagopa.fdr.repository.entity.flow;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
+import io.quarkus.mongodb.panache.PanacheQuery;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import io.quarkus.panache.common.Parameters;
+import io.quarkus.panache.common.Sort;
+import it.gov.pagopa.fdr.repository.enums.FdrStatusEnum;
+import java.time.Instant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@MongoEntity(collection = "fdr_flow")
+public class FdrFlowEntity extends PanacheMongoEntity {
+
+  private String name;
+
+  private Long revision;
+
+  @BsonProperty("fdr_date")
+  private Instant fdrDate;
+
+  private FdrStatusEnum status;
+
+  private Instant created;
+
+  private Instant updated;
+
+  private Instant published;
+
+  @BsonProperty("tot_amount")
+  private Double totAmount;
+
+  @BsonProperty("tot_payments")
+  private Long totPayments;
+
+  @BsonProperty("computed_tot_amount")
+  private Double computedTotAmount;
+
+  @BsonProperty("computed_tot_payments")
+  private Long computedTotPayments;
+
+  private String regulation;
+
+  @BsonProperty("regulation_date")
+  private Instant regulationDate;
+
+  @BsonProperty("bic_code_pouring_bank")
+  private String bicCodePouringBank;
+
+  private SenderEntity sender;
+
+  private ReceiverEntity receiver;
+
+  public ObjectId getObjectId() {
+    return this.id;
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndPspId(String name, String pspId) {
+    return find(
+        "name = :name and sender.psp_id = :pspId",
+        Parameters.with("name", name).and("pspId", pspId).map());
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndPspId(
+      String name, String pspId, Sort sort) {
+    return find(
+        "name = :name and sender.psp_id = :pspId",
+        sort,
+        Parameters.with("name", name).and("pspId", pspId).map());
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndPspIdAndOrganizationId(
+      String name, String pspId, String organizationId) {
+    return find(
+        "name = :name and sender.psp_id = :pspId and receiver.organization_id = :organizationId",
+        Parameters.with("name", name)
+            .and("pspId", pspId)
+            .and("organizationId", organizationId)
+            .map());
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndRevAndPspIdAndOrganizationId(
+      String name, Long rev, String pspId, String organizationId) {
+    return find(
+        "name = :name and sender.psp_id = :pspId and revision = :rev and receiver.organization_id ="
+            + " :organizationId",
+        Parameters.with("name", name)
+            .and("pspId", pspId)
+            .and("rev", rev)
+            .and("organizationId", organizationId)
+            .map());
+  }
+}
---
+++ src/main/java/it/gov/pagopa/fdr/repository/entity/payment/FdrPaymentEntity.java
@@ -0,0 +1,145 @@
+package it.gov.pagopa.fdr.repository.entity.payment;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
+import io.quarkus.mongodb.panache.PanacheQuery;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import io.quarkus.panache.common.Parameters;
+import io.quarkus.panache.common.Sort;
+import it.gov.pagopa.fdr.repository.enums.PaymentStatusEnum;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+import org.eclipse.microprofile.faulttolerance.Retry;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@MongoEntity(collection = "fdr_payment")
+public class FdrPaymentEntity extends PanacheMongoEntity {
+
+  private String iuv;
+
+  private String iur;
+
+  private Long index;
+
+  private Double amount;
+
+  @BsonProperty("pay_status")
+  private PaymentStatusEnum payStatus;
+
+  @BsonProperty("pay_date")
+  private Instant payDate;
+
+  @BsonProperty("transfer_id")
+  private Long transferId;
+
+  private Instant created;
+
+  private Instant updated;
+
+  @BsonProperty("ref_fdr")
+  private ReferencedFdrEntity refFdr;
+
+  public static void persistFdrPaymentHistoryEntities(
+      List<FdrPaymentEntity> fdrPaymentHistoryEntities) {
+    persist(fdrPaymentHistoryEntities);
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndIndexes(
+      ObjectId fdrId, List<Long> indexList) {
+    return find(
+        "ref_fdr.id = :fdrId and index in :indexes",
+        Parameters.with("fdrId", fdrId).and("indexes", indexList).map());
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndPspId(String fdr, String pspId) {
+    return find(
+        "ref_fdr = :fdr and ref_fdr_sender_psp_id = :pspId",
+        Parameters.with("fdr", fdr).and("pspId", pspId).map());
+  }
+
+  // https://quarkus.io/guides/smallrye-fault-tolerance
+  @Retry(delay = 500, delayUnit = ChronoUnit.MILLIS)
+  public static long deleteByFdrAndIndexes(String fdr, List<Long> indexList) {
+    return delete(
+        "ref_fdr = :fdr and index in :indexes",
+        Parameters.with("fdr", fdr).and("indexes", indexList).map());
+  }
+
+  public static long deleteByFdrAndPspId(String fdr, String pspId) {
+    return delete(
+        "ref_fdr = :fdr and ref_fdr_sender_psp_id = :pspId",
+        Parameters.with("fdr", fdr).and("pspId", pspId).map());
+  }
+
+  public static long deleteByFdr(String fdr) {
+    return delete("ref_fdr = :fdr", Parameters.with("fdr", fdr).map());
+  }
+
+  public static void persistFdrPaymentsInsert(
+      List<FdrPaymentInsertEntity> fdrPaymentInsertEntityList) {
+    persist(fdrPaymentInsertEntityList);
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndPspIAndOrganizationIdSort(
+      String fdr, String pspId, String organizationId, Sort sort) {
+    return find(
+        "ref_fdr = :fdr and ref_fdr_sender_psp_id = :pspId and ref_fdr_receiver_organization_id ="
+            + " :organizationId",
+        sort,
+        Parameters.with("fdr", fdr)
+            .and("pspId", pspId)
+            .and("organizationId", organizationId)
+            .map());
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByFdrAndRevAndPspIdAndOrganizationId(
+      String fdr, Long rev, String pspId, String organizationId, Sort sort) {
+    return find(
+        "ref_fdr = :fdr and ref_fdr_revision = :rev and ref_fdr_sender_psp_id = :pspId and"
+            + " ref_fdr_receiver_organization_id = :organizationId",
+        sort,
+        Parameters.with("fdr", fdr)
+            .and("rev", rev)
+            .and("pspId", pspId)
+            .and("organizationId", organizationId)
+            .map());
+  }
+
+  public static PanacheQuery<PanacheMongoEntityBase> findByPspAndIuvIur(
+      String psp, String iuv, String iur, Instant createdFrom, Instant createdTo, Sort sort) {
+    String query = "ref_fdr_sender_psp_id = :psp";
+    Parameters params = new Parameters().and("psp", psp);
+    if (iuv != null && iur != null) {
+      query += " and iuv = :iuv and iur = :iur";
+      params.and("iuv", iuv).and("iur", iur);
+    } else if (iuv != null) {
+      query += " and iuv = :iuv";
+      params.and("iuv", iuv);
+    } else if (iur != null) {
+      query += " and iur = :iur";
+      params.and("iur", iur);
+    }
+    if (createdFrom != null && createdTo != null) {
+      query += " and created >= :createdFrom and created <= :createdTo";
+      params.and("createdFrom", createdFrom).and("createdTo", createdTo);
+    } else if (createdFrom != null) {
+      query += " and created >= :createdFrom";
+      params.and("createdFrom", createdFrom);
+    } else if (createdTo != null) {
+      query += " and created <= :createdTo";
+      params.and("createdTo", createdTo);
+    }
+    return find(query, sort, params);
+  }
+
+  public static void persistFdrPaymentPublishEntities(
+      List<FdrPaymentEntity> fdrPaymentPublishEntities) {
+    persist(fdrPaymentPublishEntities);
+  }
+}
