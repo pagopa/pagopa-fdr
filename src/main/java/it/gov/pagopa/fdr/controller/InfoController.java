8f30373bffd444bd8b57a1a77707797bab205aa6
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-01-17 16:08:23 +0100

    refactor: re-organized health check controller

--- src/main/java/it/gov/pagopa/fdr/controller/InfoController.java
+++ src/main/java/it/gov/pagopa/fdr/controller/InfoController.java
@@ -1,21 +1,10 @@
 package it.gov.pagopa.fdr.controller;
 
+import it.gov.pagopa.fdr.controller.interfaces.IInfoController;
 import it.gov.pagopa.fdr.controller.model.common.response.InfoResponse;
 import it.gov.pagopa.fdr.service.HealthCheckService;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-@Path("/info")
-@Tag(name = "Info", description = "Info operations")
-public class InfoController {
+public class InfoController implements IInfoController {
 
   private final HealthCheckService healthCheckService;
 
@@ -23,20 +12,7 @@ public class InfoController {
     this.healthCheckService = healthCheckService;
   }
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  @Operation(summary = "Get health check and deployment-related information")
-  @APIResponses(
-      value = {
-        @APIResponse(ref = "#/components/responses/InternalServerError"),
-        @APIResponse(
-            responseCode = "200",
-            description = "Success",
-            content =
-                @Content(
-                    mediaType = MediaType.APPLICATION_JSON,
-                    schema = @Schema(implementation = InfoResponse.class)))
-      })
+  @Override
   public InfoResponse healthCheck() {
     return healthCheckService.getHealthInfo();
   }
---
+++ src/main/java/it/gov/pagopa/fdr/controller/interfaces/IInfoController.java
@@ -0,0 +1,34 @@
+package it.gov.pagopa.fdr.controller.interfaces;
+
+import it.gov.pagopa.fdr.controller.model.common.response.InfoResponse;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/info")
+@Tag(name = "Info", description = "Info operations")
+public interface IInfoController {
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Get health check and deployment-related information")
+  @APIResponses(
+      value = {
+        @APIResponse(ref = "#/components/responses/InternalServerError"),
+        @APIResponse(
+            responseCode = "200",
+            description = "Success",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = InfoResponse.class)))
+      })
+  InfoResponse healthCheck();
+}
