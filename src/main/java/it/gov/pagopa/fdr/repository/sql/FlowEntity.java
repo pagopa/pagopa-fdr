607fde8baae14970ce8dd87a083dca7e2d7f6870
Andrea D. <117269497+andrea-deri@users.noreply.github.com>
2025-02-03 18:16:48 +0100

    feat: [PAGOPA-2624] Making PoC about PostgreSQL use as new codebase (#80)
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

---
+++ src/main/java/it/gov/pagopa/fdr/repository/sql/FlowEntity.java
@@ -0,0 +1,124 @@
+package it.gov.pagopa.fdr.repository.sql;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Parameters;
+import io.quarkus.panache.common.Sort;
+import it.gov.pagopa.fdr.repository.enums.SenderTypeEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.Instant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "flow")
+public class FlowEntity extends PanacheEntityBase {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "flow_seq_gen")
+  @SequenceGenerator(name = "flow_seq_gen", sequenceName = "flow_sequence", allocationSize = 1)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @Column(name = "name")
+  public String name;
+
+  @Column(name = "date")
+  public Instant date;
+
+  @Column(name = "revision")
+  public Long revision;
+
+  @Column(name = "status")
+  public String status;
+
+  @Column(name = "is_latest")
+  public Boolean isLatest;
+
+  @Column(name = "sender_psp_id")
+  public String senderPspId; // domainId
+
+  @Column(name = "receiver_organization_id")
+  public String receiverOrganizationId; // domainId
+
+  @Column(name = "tot_amount")
+  public BigDecimal totAmount;
+
+  @Column(name = "tot_payments")
+  public Long totPayments;
+
+  @Column(name = "computed_tot_amount")
+  public BigDecimal computedTotAmount;
+
+  @Column(name = "computed_tot_payments")
+  public Long computedTotPayments;
+
+  @Column(name = "regulation")
+  public String regulation;
+
+  @Column(name = "regulation_date")
+  public Instant regulationDate;
+
+  @Column(name = "sender_id")
+  public String senderId;
+
+  @Column(name = "sender_psp_broker_id")
+  public String senderPspBrokerId;
+
+  @Column(name = "sender_channel_id")
+  public String senderChannelId;
+
+  @Column(name = "sender_password")
+  public String senderPassword;
+
+  @Column(name = "sender_psp_name")
+  public String senderPspName;
+
+  @Column(name = "sender_type")
+  public SenderTypeEnum senderType;
+
+  @Column(name = "receiver_id")
+  public String receiverId;
+
+  @Column(name = "receiver_organization_name")
+  public String receiverOrganizationName;
+
+  @Column(name = "bic_code_pouring_bank")
+  public String bicCodePouringBank;
+
+  @Column(name = "created")
+  public Instant created;
+
+  @Column(name = "updated")
+  public Instant updated;
+
+  @Column(name = "published")
+  public Instant published;
+
+  public void addOnComputedTotAmount(double value) {
+    this.computedTotAmount = this.computedTotAmount.add(BigDecimal.valueOf(value));
+  }
+
+  public void addOnComputedTotPayments(int value) {
+    this.computedTotPayments += value;
+  }
+
+  public static PanacheQuery<PanacheEntity> findPageByQuery(
+      String query, Sort sort, Parameters parameters) {
+    return find(query, sort, parameters.map());
+  }
+
+  public static PanacheQuery<PanacheEntity> findByQuery(String query, Parameters parameters) {
+    return find(query, parameters.map());
+  }
+}
--- src/main/java/it/gov/pagopa/fdr/service/middleware/validator/clause/ComputedPaymentsValidator.java
+++ src/main/java/it/gov/pagopa/fdr/service/middleware/validator/clause/ComputedPaymentsValidator.java
@@ -1,17 +1,19 @@
 package it.gov.pagopa.fdr.service.middleware.validator.clause;
 
-import it.gov.pagopa.fdr.repository.entity.flow.FdrFlowEntity;
+import it.gov.pagopa.fdr.repository.sql.FlowEntity;
 import it.gov.pagopa.fdr.util.error.enums.AppErrorCodeMessageEnum;
 import it.gov.pagopa.fdr.util.validator.ValidationArgs;
 import it.gov.pagopa.fdr.util.validator.ValidationResult;
 import it.gov.pagopa.fdr.util.validator.ValidationStep;
+import java.math.BigDecimal;
 
 public class ComputedPaymentsValidator extends ValidationStep {
 
   @Override
   public ValidationResult validate(ValidationArgs args) {
 
-    FdrFlowEntity flow = args.getArgument("flow", FdrFlowEntity.class);
+    // FdrFlowEntity flow = args.getArgument("flow", FdrFlowEntity.class);
+    FlowEntity flow = args.getArgument("flow", FlowEntity.class);
 
     // check if computed total payments is equals to pre-set total payments
     Long totPayments = flow.getTotPayments();
@@ -25,8 +27,10 @@ public class ComputedPaymentsValidator extends ValidationStep {
     }
 
     // check if computed total amount is equals to pre-set total amount
-    Double totAmount = flow.getTotAmount();
-    Double computedTotAmount = flow.getComputedTotAmount();
+    // Double totAmount = flow.getTotAmount();
+    // Double computedTotAmount = flow.getComputedTotAmount();
+    BigDecimal totAmount = flow.getTotAmount();
+    BigDecimal computedTotAmount = flow.getComputedTotAmount();
     if (!totAmount.equals(computedTotAmount)) {
       return ValidationResult.asInvalid(
           AppErrorCodeMessageEnum.REPORTING_FLOW_WRONG_SUM_PAYMENT,
