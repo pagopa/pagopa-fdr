48410cd0e9b2083ea677c590b195177f5830fba9
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2025-03-26 09:55:18 +0100

    feat: using mapping method from separated mapper class

--- src/main/java/it/gov/pagopa/fdr/schedule/HistoryCron.java
+++ src/main/java/it/gov/pagopa/fdr/schedule/HistoryCron.java
@@ -2,9 +2,6 @@ package it.gov.pagopa.fdr.schedule;
 
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.scheduler.Scheduled;
-import it.gov.pagopa.fdr.controller.model.flow.Receiver;
-import it.gov.pagopa.fdr.controller.model.flow.Sender;
-import it.gov.pagopa.fdr.controller.model.flow.enums.SenderTypeEnum;
 import it.gov.pagopa.fdr.repository.FlowRepository;
 import it.gov.pagopa.fdr.repository.FlowToHistoryRepository;
 import it.gov.pagopa.fdr.repository.PaymentRepository;
@@ -12,6 +9,7 @@ import it.gov.pagopa.fdr.repository.entity.FlowEntity;
 import it.gov.pagopa.fdr.repository.entity.FlowToHistoryEntity;
 import it.gov.pagopa.fdr.repository.entity.PaymentEntity;
 import it.gov.pagopa.fdr.storage.HistoryBlobStorageService;
+import it.gov.pagopa.fdr.storage.middleware.FlowBlobMapper;
 import it.gov.pagopa.fdr.storage.model.FlowBlob;
 import it.gov.pagopa.fdr.storage.model.PaymentBlob;
 import it.gov.pagopa.fdr.util.error.enums.AppErrorCodeMessageEnum;
@@ -29,10 +27,11 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @ApplicationScoped
 public class HistoryCron {
 
+  private final HistoryBlobStorageService historyBlobStorageService;
   private final FlowToHistoryRepository flowToHistoryRepository;
   private final FlowRepository flowRepository;
   private final PaymentRepository paymentRepository;
-  private final HistoryBlobStorageService historyBlobStorageService;
+  private final FlowBlobMapper flowBlobMapper;
 
   @ConfigProperty(name = "schedule.history.size")
   Integer size;
@@ -45,15 +44,17 @@ public class HistoryCron {
       FlowToHistoryRepository flowToHistoryRepository,
       FlowRepository flowRepository,
       PaymentRepository paymentRepository,
-      HistoryBlobStorageService historyBlobStorageService) {
+      HistoryBlobStorageService historyBlobStorageService,
+      FlowBlobMapper flowBlobMapper) {
     this.flowToHistoryRepository = flowToHistoryRepository;
     this.flowRepository = flowRepository;
     this.paymentRepository = paymentRepository;
     this.historyBlobStorageService = historyBlobStorageService;
+    this.flowBlobMapper = flowBlobMapper;
   }
 
   /**
-   * Scheduled task to process flows for historicization every hour.
+   * Scheduled task to process flows for historicization every scheduled time.
    *
    * <p>This method retrieves the top n flows that have never been started, ordered by their
    * creation date, and processes each flow by invoking the {@link #handleFlow(FlowToHistoryEntity)}
@@ -63,14 +64,13 @@ public class HistoryCron {
    * the logic defined in {@link #handleFlow(FlowToHistoryEntity)}.
    */
   @Scheduled(cron = "${schedule.history.cron}")
-  // every = "1h")
   void execute() {
 
     // retrieve the first n flows to historicize
     try {
       PanacheQuery<FlowToHistoryEntity> flows =
           flowToHistoryRepository.findTopNEntitiesOrderByCreated(size, maxRetries);
-      for (var flow : flows.list()) {
+      for (FlowToHistoryEntity flow : flows.list()) {
         handleFlow(flow);
       }
     } catch (Exception e) {
@@ -90,7 +90,7 @@ public class HistoryCron {
   private void handleFlow(FlowToHistoryEntity flowToHistory)
       throws ValidationException, IOException {
     try {
-      var flow =
+      FlowEntity flow =
           flowRepository
               .findByPspIdAndNameAndRevision(
                   flowToHistory.getPspId(), flowToHistory.getName(), flowToHistory.getRevision())
@@ -101,9 +101,9 @@ public class HistoryCron {
                           AppErrorCodeMessageEnum.REPORTING_FLOW_NOT_FOUND,
                           flowToHistory.getName()));
 
-      var payments = handlePage(flow.getId());
+      List<PaymentBlob> payments = handlePage(flow.getId());
 
-      var flowBlob = mapToFlowBlob(flow, payments);
+      FlowBlob flowBlob = flowBlobMapper.toFlowBlob(flow, payments);
       historyBlobStorageService.saveJsonFile(flowBlob);
 
       flowToHistoryRepository.deleteByIdTransactional(flowToHistory.getId());
@@ -115,40 +115,6 @@ public class HistoryCron {
       }
       throw e;
     }
-  }
-
-  private static FlowBlob mapToFlowBlob(FlowEntity flow, List<PaymentBlob> payments) {
-    return FlowBlob.builder()
-        .fdr(flow.name)
-        .fdrDate(flow.date)
-        .revision(flow.revision)
-        .created(flow.created)
-        .published(flow.published)
-        .updated(flow.updated)
-        .status(flow.status)
-        .sender(
-            Sender.builder()
-                .type(SenderTypeEnum.valueOf(flow.senderType))
-                .id(flow.senderId)
-                .pspId(flow.pspDomainId)
-                .pspName(flow.senderPspName)
-                .pspBrokerId(flow.senderPspBrokerId)
-                .channelId(flow.senderChannelId)
-                .password(flow.senderPassword)
-                .build())
-        .receiver(
-            Receiver.builder()
-                .id(flow.receiverId)
-                .organizationId(flow.orgDomainId)
-                .organizationName(flow.receiverOrganizationName)
-                .build())
-        .regulation(flow.regulation)
-        .regulationDate(flow.regulationDate.toString())
-        .bicCodePouringBank(flow.bicCodePouringBank)
-        .computedTotPayments(flow.computedTotPayments)
-        .computedSumPayments(flow.computedTotAmount)
-        .payments(payments)
-        .build();
   }
 
   private void updateFlowToHistory(FlowToHistoryEntity flowToHistory) {
@@ -163,22 +129,11 @@ public class HistoryCron {
     PanacheQuery<PaymentEntity> payments;
     do {
       payments = paymentRepository.findPageByFlowId(flowId, page, 100);
-      var paymentsBlob = payments.stream().map(HistoryCron::mapPaymentBlob).toList();
+      List<PaymentBlob> paymentsBlob =
+          payments.stream().map(flowBlobMapper::toPaymentBlob).toList();
       result.addAll(paymentsBlob);
       page++;
     } while (payments.hasNextPage());
     return result;
-  }
-
-  private static PaymentBlob mapPaymentBlob(PaymentEntity elem) {
-    return PaymentBlob.builder()
-        .index(elem.getIndex())
-        .iur(elem.getIur())
-        .iuv(elem.getIuv())
-        .idTransfer(elem.getTransferId())
-        .pay(elem.getAmount())
-        .payDate(elem.getPayDate().toString())
-        .payStatus(elem.getPayStatus())
-        .build();
   }
 }
