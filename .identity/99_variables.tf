9150ea3924fe74c5c3b21b976b00ec5f05f056d2
svariant <samuele.varianti@nttdata.com>
2025-03-27 16:43:40 +0100

    fix db host

--- .identity/99_variables.tf
+++ .identity/99_variables.tf
@@ -28,7 +28,7 @@ locals {
   }
 
   postgres_db = {
-    host = "pagopa-${var.env_short}-weu-fdr-flexible-postgresql.postgres.database.azure.com"
+    host = "pagopa-${var.env_short}-weu-fdr-flexible-postgresql"
     port = 5432
     name = "fdr3"
     schema = "fdr3"
