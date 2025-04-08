2521a3d94b45225cee030c302bbb8bc3ff9dd768
svariant <samuele.varianti@nttdata.com>
2025-04-08 15:46:10 +0200

    fix: [PAGOPA-2978] DB script path

--- src/main/resources/db/migration/liquibase/changelog/1/db.changelog-202504081545.sql
+++ src/main/resources/db/migration/liquibase/changelog/1/db.changelog-202504081545.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
 
 -- ## SEQUENCES ##
---changeset liquibase:1738839600000-01
+--changeset liquibase:202504081545-01
 ALTER TABLE fdr3.flow ALTER COLUMN tot_amount TYPE float8 USING tot_amount::float8;
---changeset liquibase:1738839600000-02
+--changeset liquibase:202504081545-02
 ALTER TABLE fdr3.flow ALTER COLUMN computed_tot_amount TYPE float8 USING computed_tot_amount::float8;
---changeset liquibase:1738839600000-03
+--changeset liquibase:202504081545-03
 ALTER TABLE fdr3.payment ALTER COLUMN amount TYPE float8 USING amount::float8;
