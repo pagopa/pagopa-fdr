6cf0feb1fbdfed58d12dc1ac685798200d57d5b7
giomella <giomella@emeal.nttdata.com>
2025-10-07 10:21:40 +0200

    [PAGOPA-1037] update index

--- src/main/resources/db/migration/liquibase/changelog/1.0.3/db.changelog-202510060000.sql
+++ src/main/resources/db/migration/liquibase/changelog/1.0.3/db.changelog-202510060000.sql
@@ -4,7 +4,7 @@
 --changeset liquibase:202510060000-01
 CREATE INDEX IF NOT EXISTS orgid_by_status_latest_idx
     ON fdr3.flow USING btree
-    (org_domain_id COLLATE pg_catalog."default" ASC NULLS LAST, status COLLATE pg_catalog."default" ASC NULLS LAST, is_latest ASC NULLS LAST, date DESC NULLS LAST)
+    (org_domain_id COLLATE pg_catalog."default" ASC NULLS LAST, status COLLATE pg_catalog."default" ASC NULLS LAST, is_latest ASC NULLS LAST, date ASC NULLS LAST)
     WITH (fillfactor=100, deduplicate_items=True)
     TABLESPACE pg_default;
 --changeset liquibase:202510060000-02
