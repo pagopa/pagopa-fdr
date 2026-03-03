64466c9f2e5bcae17365fb2b88c5704ee34f1852
Francesco <cesareo.francesco@gmail.com>
2026-03-03 16:47:16 +0100

    restore

--- src/main/resources/db/migration/liquibase/admin-changelog/1.0.0/db.changelog-202603030000.sql
+++ src/main/resources/db/migration/liquibase/admin-changelog/1.0.0/db.changelog-202603030000.sql
@@ -5,11 +5,13 @@
 CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 --changeset liquibase:admin-202603030000-02
-GRANT USAGE ON SCHEMA cron TO azureuser;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA cron TO azureuser;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA cron TO azureuser;
+-- GRANT USAGE ON SCHEMA cron TO azureuser;
+-- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA cron TO azureuser;
+-- GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA cron TO azureuser;
+GRANT azure_pg_admin TO cron;
 
 --changeset liquibase:admin-202603030000-03 endDelimiter:GO
 -- cron.schedule(job_name, schedule, command, database, username, active)
-SELECT cron.schedule('job_move_published_payments', '*/10 * * * *', $$call fdr3.move_published_payments(10);$$);
+-- SELECT cron.schedule('job_move_published_payments', '*/10 * * * *', $$call fdr3.move_published_payments(10);$$);
+SELECT cron.schedule_in_database('job_move_published_payments', '*/10 * * * *', $$call fdr3.move_published_payments(10);$$,'fdr3');
 GO
