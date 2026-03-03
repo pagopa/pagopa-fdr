579ac260f07b6f670c60cc4d12e7de94f6672378
Francesco <cesareo.francesco@gmail.com>
2026-03-03 12:51:13 +0100

    fix

--- src/main/resources/db/migration/liquibase/admin-changelog/1.0.0/db.changelog-202603030000.sql
+++ src/main/resources/db/migration/liquibase/admin-changelog/1.0.0/db.changelog-202603030000.sql
@@ -3,6 +3,7 @@
 -- ## SEQUENCES ##
 --changeset liquibase:admin-202603030000-01
 GRANT USAGE ON SCHEMA cron TO fdr3;
---changeset liquibase:admin-202603030000-02
+--changeset liquibase:admin-202603030000-02 endDelimiter:GO
 -- cron.schedule_in_database(job_name, schedule, command, database, username, active)
 SELECT cron.schedule_in_database('job_move_published_payments', '/10 * * * *', $$call fdr3.move_published_payments();$$,'fdr3', 'fdr3', true);
+GO
