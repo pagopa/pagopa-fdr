311e479a91709bbae7c21a599939e70965564973
Francesco <cesareo.francesco@gmail.com>
2026-03-11 18:15:42 +0100

    fix db

--- src/main/resources/db/migration/liquibase/admin-changelog/1.0.0/db.changelog-202603030000.sql
+++ src/main/resources/db/migration/liquibase/admin-changelog/1.0.0/db.changelog-202603030000.sql
@@ -6,5 +6,5 @@ CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 --changeset liquibase:admin-202603030000-02 endDelimiter:GO
 -- cron.schedule_in_database(job_name, schedule, command, database, username, active)
-SELECT cron.schedule_in_database('job_move_published_payments', '*/1 * * * *', $$call fdr3.move_published_payments(10);$$,'fdr3');
+SELECT cron.schedule_in_database('job_move_published_payments', '*/1 * * * *', $$call fdr3.move_published_payments(1);$$,'fdr3');
 GO
