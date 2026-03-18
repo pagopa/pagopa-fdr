45538e6194f44919a8b9bd8d6e5ed2927c385e73
andrea-deri <andrea.derinaldis@pagopa.it>
2026-03-18 16:30:22 +0100

    [PAGOPA-3484] feat: Adding configuration for data retention and process scheduling on online DB

---
+++ src/main/resources/db/migration/liquibase/online/azureuser.changelog/1.1.1/db.changelog-202603110002.sql
@@ -0,0 +1,25 @@
+--liquibase formatted sql
+
+-- ## MODULES ##
+--changeset liquibase:azureuser-202603110002-01
+CREATE SERVER IF NOT EXISTS remote_postgres
+       FOREIGN DATA WRAPPER postgres_fdw
+       OPTIONS (host '${fdr3-online-host}', port '${fdr3-online-port}', dbname 'postgres', sslmode 'require');
+
+CREATE USER MAPPING IF NOT EXISTS
+       FOR azureuser
+       SERVER remote_postgres
+       OPTIONS (user 'azureuser', password '${azureuser-online-password}');
+
+-- ## GRANTS ##
+--changeset liquibase:azureuser-202603110002-02
+GRANT USAGE
+      ON FOREIGN SERVER remote_postgres
+      TO azureuser;
+
+-- ## TABLES ##
+--changeset liquibase:azureuser-202603110002-03
+IMPORT FOREIGN SCHEMA cron
+       LIMIT TO (job_run_details)
+       FROM SERVER remote_postgres
+       INTO maintenance;
