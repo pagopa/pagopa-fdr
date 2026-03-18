ae5c65e006aeb701ce8ec95f035e63f1e6ef867d
andrea-deri <andrea.derinaldis@pagopa.it>
2026-03-18 16:23:22 +0100

    [PAGOPA-3484] feat: Including FDW for data archiving on cron's log table

--- src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.0/db.changelog-202603090003.sql
+++ src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.0/db.changelog-202603090003.sql
@@ -15,8 +15,18 @@ CREATE USER MAPPING IF NOT EXISTS
        SERVER remote_fdr3
        OPTIONS (user 'azureuser', password '${fdr3-online-password}');
 
--- ## GRANTS ##
 --changeset liquibase:archive-azureuser-202603090003-03
+CREATE SERVER IF NOT EXISTS remote_postgres
+       FOREIGN DATA WRAPPER postgres_fdw
+       OPTIONS (host '${fdr3-online-host}', port '${fdr3-online-port}', dbname 'postgres', sslmode 'require');
+
+CREATE USER MAPPING IF NOT EXISTS
+       FOR azureuser
+       SERVER remote_postgres
+       OPTIONS (user 'azureuser', password '${azureuser-online-password}');
+
+-- ## GRANTS ##
+--changeset liquibase:archive-azureuser-202603090003-04
 GRANT USAGE
       ON FOREIGN SERVER remote_fdr3
       TO azureuser;
@@ -25,9 +35,19 @@ GRANT USAGE
       ON FOREIGN SERVER remote_fdr3
       TO fdr3;
 
+GRANT USAGE
+      ON FOREIGN SERVER remote_postgres
+      TO azureuser;
+
 -- ## TABLES ##
---changeset liquibase:archive-azureuser-202603090003-04
+--changeset liquibase:archive-azureuser-202603090003-05
 IMPORT FOREIGN SCHEMA fdr3
        LIMIT TO (mview_flows_published_last_day, mview_payments_published_last_day)
        FROM SERVER remote_fdr3
        INTO remote_fdr3;
+
+ --changeset liquibase:archive-azureuser-202603110002-06
+ IMPORT FOREIGN SCHEMA cron
+        LIMIT TO (job_run_details)
+        FROM SERVER remote_postgres
+        INTO maintenance;
