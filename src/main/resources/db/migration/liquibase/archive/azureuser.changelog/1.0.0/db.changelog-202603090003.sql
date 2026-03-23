eb6954a1837a5881b47a859d199e3e85644ffaa2
andrea-deri <andrea.derinaldis@pagopa.it>
2026-03-23 18:42:00 +0100

    [PAGOPA-3484] fix: Changing wrong changeset identifier

--- src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.0/db.changelog-202603090003.sql
+++ src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.0/db.changelog-202603090003.sql
@@ -46,7 +46,7 @@ IMPORT FOREIGN SCHEMA fdr3
        FROM SERVER remote_fdr3
        INTO remote_fdr3;
 
---changeset liquibase:archive-azureuser-202603110002-06
+--changeset liquibase:archive-azureuser-202603090003-06
 IMPORT FOREIGN SCHEMA cron
        LIMIT TO (job_run_details)
        FROM SERVER remote_postgres
