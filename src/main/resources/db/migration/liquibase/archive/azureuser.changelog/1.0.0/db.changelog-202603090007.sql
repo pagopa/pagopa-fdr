7343806509fd74144f7bfb39a64fa5bbf0662696
andrea-deri <andrea.derinaldis@pagopa.it>
2026-03-24 10:43:21 +0100

    [PAGOPA-3484] fix: Resolving wrong column name

--- src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.0/db.changelog-202603090007.sql
+++ src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.0/db.changelog-202603090007.sql
@@ -31,7 +31,7 @@ INSERT INTO maintenance.partition_config (schema_name, table_name, retention_typ
             ,('fdr3', 'payment', 'month', 6, 'Y');
 
 --changeset liquibase:admin-archive-202603090007-03
-INSERT INTO maintenance.archive_config (archive_type, src_schema_name, src_table_name, dst_schema_name, dst_table_name, is_active, batch_size, chunk_column, partition_date_column, execution_order)
+INSERT INTO maintenance.archive_config (archive_type, src_schema_name, src_table_name, dst_schema_name, dst_table_name, is_active, batch_size, batch_column, partition_date_column, execution_order)
      VALUES ('daily', 'remote_fdr3', 'mview_flows_published_last_day', 'fdr3', 'flow', 'Y', 50000, 'id', 'date', 1)
             ,('daily', 'remote_fdr3', 'mview_payments_published_last_day', 'fdr3', 'payment', 'Y', 1000, 'flow_id', 'flow_date', 2);
 
