54a3db602bac7df5e746ae7ff26f165fc740a1c4
andrea-deri <andrea.derinaldis@pagopa.it>
2026-03-25 15:41:42 +0100

    [PAGOPA-3484] fix: Moving SQL scripts on separated changelogs

--- src/main/resources/db/migration/liquibase/online/azureuser.changelog/1.1.3/db.changelog-202603120001.sql
+++ src/main/resources/db/migration/liquibase/online/azureuser.changelog/1.1.3/db.changelog-202603120001.sql
@@ -1,5 +1,11 @@
 --liquibase formatted sql
 
+-- =============================================
+--                    NOTE
+-- This script MUST be executed on "postgres"
+--  database in "online" PGFlex server.
+-- =============================================
+
 -- ## CRON SCHEDULES ##
 --changeset liquibase:azureuser-202603120001-01
 SELECT cron.schedule_in_database
@@ -17,11 +23,3 @@ SELECT cron.schedule_in_database
         ,'00 02 * * *'
         ,$$CALL maintenance.execute_data_cleansing();$$
         ,'fdr3');
-
--- ## CONFIGURATION ##
---changeset liquibase:azureuser-202603120001-02
-INSERT INTO maintenance.retention_config (schema_name, table_name, is_active, retention_type, retention, batch_column, batch_size, retention_date_column, execution_order)
-     VALUES ('maintenance', 'process_log', 'Y', 'day', 7, 'id', 5000, 'date', 1)
-            ,('maintenance', 'cron_aux', 'Y', 'day', 7, 'id', 5000, 'start_time', 2)
-            ,('maintenance', 'job_run_details', 'Y', 'day', 7, 'runid', 5000, 'start_time', 3) -- via postgres_fdw
-            ,('fdr3', 'flow', 'Y', 'day', 35, 'id', 1000, 'date', 4); -- also cascading payment table
