d9b5803dde7efb3cab2114092fac151f7150aa07
Francesco <cesareo.francesco@gmail.com>
2026-03-25 11:42:50 +0100

    clean code

--- src/main/java/it/gov/pagopa/fdr/controller/InternalPspsOperationsController.java
+++ src/main/java/it/gov/pagopa/fdr/controller/InternalPspsOperationsController.java
@@ -4,20 +4,14 @@ import it.gov.pagopa.fdr.controller.interfaces.annotation.Re;
 import it.gov.pagopa.fdr.controller.interfaces.controller.IInternalPspsOperationsController;
 import it.gov.pagopa.fdr.controller.model.common.response.GenericResponse;
 import it.gov.pagopa.fdr.controller.model.flow.request.CreateFlowRequest;
-import it.gov.pagopa.fdr.controller.model.flow.response.PaginatedFlowsResponse;
 import it.gov.pagopa.fdr.controller.model.flow.response.SingleFlowCreatedResponse;
 import it.gov.pagopa.fdr.controller.model.payment.request.InternalAddPaymentRequest;
 import it.gov.pagopa.fdr.controller.model.payment.request.InternalDeletePaymentRequest;
 import it.gov.pagopa.fdr.service.FlowService;
 import it.gov.pagopa.fdr.service.PaymentService;
-import it.gov.pagopa.fdr.service.model.arguments.FindFlowsByFiltersArgs;
 import it.gov.pagopa.fdr.service.model.re.FdrActionEnum;
 import jakarta.ws.rs.core.Response.Status;
 import org.jboss.resteasy.reactive.RestResponse;
-
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.util.Optional;
 
 public class InternalPspsOperationsController implements IInternalPspsOperationsController {
 
@@ -76,19 +70,4 @@ public class InternalPspsOperationsController implements IInternalPspsOperations
 
     return this.flowService.retrieveSingleUnpublishedFlow(organizationId, pspId, flowName);
   }
-
-//  @Override
-//  public PaginatedFlowsResponse getAllPublishedFlowsForInternalUse(String organizationId, String pspId, Optional<Instant> publishedGt, Optional<Instant> flowDate, long pageNumber, long pageSize) {
-//
-//    Instant defaultDate = Instant.now().atZone(ZoneOffset.UTC).minusMonths(1).toInstant();
-//    return this.flowService.getPaginatedPublishedFlowsForCI(
-//            FindFlowsByFiltersArgs.builder()
-//                    .organizationId(organizationId)
-//                    .pspId(pspId)
-//                    .publishedGt(publishedGt.orElse(defaultDate))
-//                    .flowDate(flowDate.orElse(defaultDate))
-//                    .pageNumber(pageNumber)
-//                    .pageSize(pageSize)
-//                    .build());
-//  }
 }
