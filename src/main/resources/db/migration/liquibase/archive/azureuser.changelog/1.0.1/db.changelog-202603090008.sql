57fbc684a7b811f992b90e06ab7ad43a97fc597d
andrea-deri <andrea.derinaldis@pagopa.it>
2026-04-07 10:04:46 +0200

    [PAGOPA-3484] fix: Reducing batch size for payment table

--- src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.1/db.changelog-202603090008.sql
+++ src/main/resources/db/migration/liquibase/archive/azureuser.changelog/1.0.1/db.changelog-202603090008.sql
@@ -9,7 +9,7 @@ INSERT INTO maintenance.partition_config (schema_name, table_name, retention_typ
 --changeset liquibase:archive-azureuser-202603090008-02
 INSERT INTO maintenance.archive_config (archive_type, src_schema_name, src_table_name, dst_schema_name, dst_table_name, is_active, batch_size, batch_column, partition_date_column, execution_order)
      VALUES ('daily', 'remote_online_db', 'mview_flows_published_last_day', 'fdr3', 'flow', 'Y', 50000, 'id', 'date', 1)
-            ,('daily', 'remote_online_db', 'mview_payments_published_last_day', 'fdr3', 'payment', 'Y', 1000, 'flow_id', 'flow_date', 2);
+            ,('daily', 'remote_online_db', 'mview_payments_published_last_day', 'fdr3', 'payment', 'Y', 1, 'flow_id', 'flow_date', 2); -- batching by all payments in flow_id
 
 --changeset liquibase:archive-azureuser-202603090008-03
 INSERT INTO maintenance.retention_config (schema_name, table_name, is_active, retention_type, retention, batch_column, batch_size, retention_date_column, execution_order)
